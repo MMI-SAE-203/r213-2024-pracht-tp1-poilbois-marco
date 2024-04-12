9570370ad2594bbe56a1e92690ed72f98e46e1cf
poilbois-marco <marco.poilbois@edu.univ-fcomte.fr>
2024-04-12 08:22:53 +0200

    test avec Vue.js Devtools

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,8 @@
 <script setup lang="ts">
-import { onErrorCaptured } from 'vue'
+import { onErrorCaptured, ref } from 'vue'
 import { RouterLink, RouterView } from 'vue-router/auto'
+
+const menuIsOpen = ref(false)
 
 onErrorCaptured((err, instance, info) => {
   console.error('erreur : ', err, '\ninfo : ', info, '\ncomposant : ', instance)
